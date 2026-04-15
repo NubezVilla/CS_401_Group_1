f762936db571d2e57b421e1673b3caa3dfdc74eb
Jack Merrell <jademe722@gmail.com>
2026-04-15 04:57:34 -0700

    Implemented User and Message, started Conversation

--- src/model/Conversation.java
+++ src/model/Conversation.java
@@ -1,7 +1,44 @@
 package model;
 
 import java.io.Serializable;
-
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashSet;
+//Important!! Must read Conversations from file in ID order, as ID cannot be manually set. 
 public class Conversation implements Serializable {
-
+	private static int count = 0;
+	private String conversationID;
+	private HashSet<String> participantIDs;
+	private ArrayList<Message> messages;
+	private Instant createdAt;
+	private Instant mostRecentMessage;
+	//default constructor
+	//we may need more constructors, not sure
+	public Conversation() {
+		conversationID = Integer.toString(++count);
+		createdAt = Instant.now();
+		participantIDs = new HashSet<String>();
+		messages = new ArrayList<Message>();
+		mostRecentMessage = null;
+	}
+	
+	public String getID() {
+		return conversationID;
+	}
+	
+	public HashSet<String> getParticipants(){
+		return participantIDs;
+	}
+	
+	public ArrayList<Message> getMessages(){
+		return messages;
+	}
+	
+	public Instant getCreated() {
+		return createdAt;
+	}
+	
+	public Instant getMostRecentMessageTimestamp() {
+		return mostRecentMessage;
+	}
 }
