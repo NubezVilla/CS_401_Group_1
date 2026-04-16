4cb870dd46e50b78ca93955972e7ce890f54271a
Jack Merrell <jademe722@gmail.com>
2026-04-16 14:45:15 -0700

    All data model classes implemented.
    Conversation/GC probably need a second pass.

--- src/model/GroupConversation.java
+++ src/model/GroupConversation.java
@@ -1,5 +1,38 @@
 package model;
 
-public class GroupConversation extends Conversation {
+import java.time.Instant;
 
+public class GroupConversation extends Conversation {
+	private String groupName;
+	private String creatorID;
+	
+	public GroupConversation(Conversation base, String creatorID) {
+		for (String p : base.getParticipants()) {
+			this.addParticipant(p);
+		}
+		this.creatorID = creatorID;
+	}
+	
+	public GroupConversation(Conversation base, String creatorID, Instant created){
+		super(created);
+		for (String p : base.getParticipants()) {
+			this.addParticipant(p);
+		}
+		this.creatorID = creatorID;
+	}
+	
+	public String getName() {
+		return groupName;
+	}
+	
+	public String getCreator() {
+		return creatorID;
+	}
+	
+	public void setName(String  n) {
+		groupName = n;
+	}
+	
+	
+	
 }
--- src/model/Report.java
+++ src/model/Report.java
@@ -1,5 +1,20 @@
 package model;
 
-public class Report extends Message {
+import java.time.Instant;
 
+public class Report extends Message {
+	private Boolean resolved;
+	public Report (String text, String senderID) {
+		super(text, senderID);
+		resolved = false;
+	}
+	
+	public Report (String text, String senderID, Instant timestamp) {
+		super(text, senderID, timestamp);
+		resolved = false;
+	}
+	
+	public void resolve() {
+		resolved = !resolved;
+	}
 }
